d9d714e826047f7768710bb9093cfa44140b8edc
zingfind <decoyemail@duck.com>
2025-10-16 04:23:50 +0500

    Action menu

--- game.py
+++ game.py
@@ -132,51 +132,107 @@ def blockstep(key):
     else:
         pass
 
-
 def nwblock():
     print("You're in Northwest Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo1c).grid(row=1,column=0)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Нажать на первую кнопку пульта")
+    listbox.insert(1, "Нажать на вторую кнопку пульта")
+    listbox.grid(row=1,column=0)
 
 def nblock():
     print("You're in North Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo2c).grid(row=1,column=1)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Войти в систему компьютера")
+    listbox.insert(2, "Сломать компьютер")
+    listbox.grid(row=1,column=0)
 
 def neblock():
     print("You're in North Eastern Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo3c).grid(row=1,column=2)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Открыть дверь бункера")
+    listbox.grid(row=1,column=0)
 
 def wblock():
     print("You're in Western Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo4c).grid(row=2,column=0)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Разобраться в мусоре на полу")
+    listbox.grid(row=1,column=0)
 
 def cblock():
     print("You're in Central Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo5c).grid(row=2,column=1)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Поспать")
+    listbox.grid(row=1,column=0)
 
 def eblock():
     print("You're in East Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo6c).grid(row=2,column=2)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Открыть шкафчики")
+    listbox.insert(2, "Потрясти шкафчики")
+    listbox.grid(row=1,column=0)
 
 def swblock():
     print("You're in South Western Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo7c).grid(row=3,column=0)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Заглянуть в цветочный горшок")
+    listbox.grid(row=1,column=0)
 
 def sblock():
     print("You're in South Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo8c).grid(row=3,column=1)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Заглянуть под ковёр")
+    listbox.grid(row=1,column=0)
 
 def seblock():
     print("You're in South Eastern Block.")
     resetmap()
     Button(frame1, text = 'button', image = photo9c).grid(row=3,column=2)
+    for widget in frame2.winfo_children():
+        widget.destroy()
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Заглянуть в цветочный горшок")
+    listbox.grid(row=1,column=0)
 
 def resetmap():
     Button(frame1, text = 'button', image = photo1).grid(row=1,column=0)
@@ -199,6 +255,10 @@ def startmap():
     Button(frame1, text = 'button', image = photo7).grid(row=3,column=0)
     Button(frame1, text = 'button', image = photo8).grid(row=3,column=1)
     Button(frame1, text = 'button', image = photo9).grid(row=3,column=2)
+    Label(frame2, text="Выбор действия").grid(row=0,column=0)
+    listbox = Listbox(frame2, width=40, height=0, font=("Arial", 10))
+    listbox.insert(1, "Поспать")
+    listbox.grid(row=1,column=0)
 
 root = tk.Tk()
 
@@ -206,14 +266,13 @@ currentblock = "C"
 
 root.bind("<Key>", blockstep)
 
-root.title("Пиксельная Фаллаут РПГ")
+root.title("Фаллаут ролевая игра")
 root.minsize(920, 460)
 root.maxsize(920, 460)
 root.geometry("+100+50")
 
 bg = PhotoImage(file = "background.png")
 
-# Show image using label
 label1 = Label(root, image = bg)
 label1.place(x = 0, y = 0)
 
@@ -222,6 +281,11 @@ frame1.grid(row=0, column=0, padx=(20, 20), pady=(20, 20))
 
 frame2 = tk.Frame(root, borderwidth=1, relief='ridge')
 frame2.grid(row=0, column=1, padx=(20, 20), pady=(20, 20))
+
+frame3 = tk.Frame(root, borderwidth=1, relief='ridge')
+frame3.grid(row=0, column=2, padx=(20, 20), pady=(20, 20))
+
+#Label(frame3, text="Инвентарь").grid(row=0,column=0)
 
 photo1 = PhotoImage(file = r"nwtile.png")
 photo2 = PhotoImage(file = r"ntile.png")
@@ -246,5 +310,3 @@ photo9c = PhotoImage(file = r"setilecharacter.png")
 startmap()
 
 root.mainloop()
-
-#I gotta set a background for the whole window as a tiled image.
